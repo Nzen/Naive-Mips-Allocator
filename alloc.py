a55d19de5522546b6dd5439f5c9ea557b4145987
Nicholas Prado <damiengrey@sbcglobal.net>
2013-06-01 21:28:43 -0700

    Passed first test
    So, because module variables are ambiguously scoped, I just shoved
    everything into a class with tears streaming. Now I need to check
    that all the rest of the control paths work. This involves blowing
    all my registers, putting blank lines, comments, tags, and a fair
    number of different instructions.
    
    As I'm using arity as a contract, I took out the intended restriction
    BSinning vim. why does left arrow branch to the line above? Commit over.
    Dto tranff
    of distinguishing tags from variable names ()

--- alloc.py
+++ alloc.py
@@ -5,152 +5,182 @@
 '''
  - instructions not included -
 bgezal bltzal b j jal jr noop negu rem remu rol ror bczf bczt jalr abs.d add.d bc1f bc1t c.eq.d c.le.d c.lt.d cvt.d.w cvt.s.d div.d l.d l.s mul.d sub.d break lwcz mtcz mthi mtlo sd sh
+
+I've been away for a while and am transitioning to py3 so all these gotchas are in my sinning way.
 '''
-instr_arity = {
-	# nor $t7, $a0, $lo
-	"add" : 3, "addu" : 3, "and" : 3,
-	"srav" : 3, "srlv" : 3, "sub" : 3,
-	"subu" : 3, "xor" : 3, "mul" : 3,
-	"mulo" : 3, "mulou" : 3, "srl" : 3,
-	"seq" : 3, "sge" : 3, "sgeu" : 3,
-	"sgt" : 3, "sgtu" : 3, "sle" : 3,
-	"sleu" : 3, "sltu" : 3, "sne" : 3,
-	"nor" : 3, "or" : 3, "sllv" : 3,
-	"slt" : 3, "sltu" : 3, "addi" : 2,
-	# lb $t, offset($s)
-	"addiu" : 2, "andi" : 2, "beq" : 2,
-	"bne" : 2, "div" : 2, "divu" : 2,
-	"lb" : 2, "lw" : 2, "mult" : 2,
-	"multu" : 2, "neg" : 2, "not" : 2,
-	"ori" : 2, "sb" : 2, "sll" : 2,
-	"slti" : 2, "sltiu" : 2, "sra" : 2,
-	"srl" : 2, "sw" : 2, "xori" : 2,
-	"bge" : 2, "bgeu" : 2, "bgt" : 2,
-	"bgtu" : 2, "ble" : 2, "bleu" : 2,
-	"blt" : 2, "bltu" : 2, "sltiu" : 2,
-	"lbu" : 2, "lh" : 2, "move" : 2,
-	# beqz $s0, further
-	"beqz" : 1, "bgez" : 1, "bgtz" : 1,
-	"blez" : 1, "bltz" : 1, "lui" : 1,
-	"mfhi" : 1, "mflo" : 1, "la" : 1,
-	"li" : 1
-	}
-reserved = {
-	"_arg0" : "$a0",
-	"_arg1" : "$a1",
-	"_arg2" : "$a2",
-	"_arg3" : "$a3",
-	"ra" : "$ra",
-	"_0" : "$0",
-	"_out1" : "$v0",
-	"_out2" : "$v1",
-	}
-tr_left = 9
-sr_left = 7
-sp_next = 4 # stack decrements down
-used = []
-#op, tar, sr1, sr2 = range( 0, 3 ) # woo, enums
-op = 0
+class Alloc :
 
-def should_trans() :
-	in = "add _pivot, _least, _pivot" # simplest first
-	expect = "add $t0, $t1, $t0 # pivot, least, pivot\n"
-			# put some context in the comment later, x = y, z
-	assert expect == trans( in ), "output didn't match expected"
+	def __init__(self) :
+		self.instr_arity = {
+			# nor $t7, $a0, $lo
+			"add" : 3, "addu" : 3, "and" : 3,
+			"srav" : 3, "srlv" : 3, "sub" : 3,
+			"subu" : 3, "xor" : 3, "mul" : 3,
+			"mulo" : 3, "mulou" : 3, "srl" : 3,
+			"seq" : 3, "sge" : 3, "sgeu" : 3,
+			"sgt" : 3, "sgtu" : 3, "sle" : 3,
+			"sleu" : 3, "sltu" : 3, "sne" : 3,
+			"nor" : 3, "or" : 3, "sllv" : 3,
+			"slt" : 3, "sltu" : 3, "addi" : 2,
+			# lb $t, offset($s)
+			"addiu" : 2, "andi" : 2, "beq" : 2,
+			"bne" : 2, "div" : 2, "divu" : 2,
+			"lb" : 2, "lw" : 2, "mult" : 2,
+			"multu" : 2, "neg" : 2, "not" : 2,
+			"ori" : 2, "sb" : 2, "sll" : 2,
+			"slti" : 2, "sltiu" : 2, "sra" : 2,
+			"srl" : 2, "sw" : 2, "xori" : 2,
+			"bge" : 2, "bgeu" : 2, "bgt" : 2,
+			"bgtu" : 2, "ble" : 2, "bleu" : 2,
+			"blt" : 2, "bltu" : 2, "sltiu" : 2,
+			"lbu" : 2, "lh" : 2, "move" : 2,
+			# beqz $s0, further
+			"beqz" : 1, "bgez" : 1, "bgtz" : 1,
+			"blez" : 1, "bltz" : 1, "lui" : 1,
+			"mfhi" : 1, "mflo" : 1, "la" : 1,
+			"li" : 1
+			}
+		self.reserved = {
+			"arg0" : "$a0",
+			"arg1" : "$a1",
+			"arg2" : "$a2",
+			"arg3" : "$a3",
+			"ra" : "$ra",
+			"_0" : "$0",
+			"out1" : "$v0",
+			"out2" : "$v1",
+			}
+		self.tr_left = 9
+		self.sr_left = 7
+		self.sp_next = 4 # stack decrements down
+		self.used = []
+		self.op = 0
 
-def auto( file ) :
-	raw = ""
-	try :
-	asm_file = open( file )
-	for line in asm_file : # more advanced means changing this or processing later
-		if line == "\n" :
-			continue
-		raw = line.rstrip( '\n' )
-		print( trans( raw ) )
-	asm_file.close()
-	except IOError :
-		print ( "File Error: Perhaps an invalid name?" )
+	def show( self ) :
+		'so my test doesnt become half test, half debugger'
+		inp = "add pivot, least, pivot"
+		got = self.trans( inp )
+		print( "got -{0}-".format( got ) ) # ugh
 
- # line is split on spaces
-def trans( line ) : # or multiple lines
-	pieces = line.split( ' ' )
-	if noop_line( pieces[op] ) :
-		return line # better to join them together from pieces?
-	elif line[op] in instr_arity :
-		return trans_line( pieces )
-	else
-		return line + " #<- no matching parse rule"
+	def should_trans( self ) :
+		inp = "add pivot, least, pivot" # simplest first
+		expect = "add $t0, $t1, $t0 # pivot, least, pivot\n"
+				# put some context in the comment later, x = y, z
+		got = self.trans( inp )
+		assert expect == got, "\n\toutput didn't match: " + got
 
-# comment, tag:, 0 arity, blank already handled
-def noop_line( symb ) :
-	first_char = symb[0] # refactor control flow later
-	last_char = symb[-1]
-	if first_char == "#" : # comment
-		return true
-	elif last_char == ":" # tag, to avoid below greedy test
-		return true
-	elif first_char == "j" # jumps
-		return true
-	else
-		return false
+	def auto( self, file ) :
+		raw = ""
+		try :
+			asm_file = open( file )
+			for line in asm_file : # more advanced means changing this or processing later
+				if line == "\n" :
+					continue
+				raw = line.rstrip( '\n' )
+				print( self.trans( raw ) )
+			asm_file.close()
+		except IOError :
+			print ( "File Error: Perhaps an invalid name?" )
 
-def trans_line( piece ) :
-	replace = instr_arity[ piece[op] ]
-	out = piece[op] + " "
-	done = []
-	which = 1
-	# not unrolling loop for now
-	while which <= replace :
-		assigned = assign( piece[ which ] )
-		out = out + assigned + " "
-		done.append( piece[which] )
-		which += 1
-	out = out + "# "
-	for sym in done :
-		out = out + sym + " "
-	if len(piece) > replace + 1 : # has comment, so put rest
-		which += 2 # skip the #, -assumes hash always has space-
-		while which < len(piece) # -1 ?
-			out = out + piece[which] + " "
-	out[-1] = '\n' # replace extraneous space
+	 # line is split on spaces
+	def trans( self, line ) : # or multiple lines
+		pieces = line.split( ' ' )
+		if self.noop_line( pieces[self.op] ) :
+			return line # better to join them together from pieces?
+		elif pieces[self.op] in self.instr_arity :
+			return self.trans_line( pieces )
+		else :
+			return line + " #<- no matching parse rule"
 
-def assign( symbol ) :
-	if symbol in reserved :
-		return reserved[ symbol ]
-	elif already_seen( symbol ) :
-		return prior_reservation( symbol )
-	elif t_reg_open() :
-		return temp_r_chosen( symbol )
-	elif s_reg_open() :
-		return sav_r_chosen( symbol )
-	else
-		return put_in_stack( symbol )
+	# comment, tag:, 0 arity, blank already handled
+	def noop_line( self, symb ) :
+		first_char = symb[0] # refactor control flow later
+		last_char = symb[-1]
+		if first_char == "#" : # comment
+			return True
+		elif last_char == ":" : # tag, to avoid below greedy test
+			return True
+		elif first_char == "j" : # jumps
+			return True
+		else :
+			return False
 
-def t_reg_open() :
-	return tr_left >= 0
-def s_reg_open() :
-	return sr_left >= 0
+	def trans_line( self, piece ) :
+		# print(piece) # woo, debugging
+		replace = self.instr_arity[ piece[self.op] ]
+		out = piece[self.op] + " "
+		done = []
+		which = 1
+		while which <= replace :
+			assigned = self.assign( piece[ which ] )
+			out = out + assigned + " "
+			done.append( piece[which] )
+			which += 1
+		if out[-2] == ',' :
+			out = out[0:-2] + " " # omit that comma
+		out = out + "# "
+		for sym in done :
+			out = out + sym + " "
+		if len(piece) > replace + 1 : # has comment, so put rest
+			which += 2 # skip the #, -assumes hash always has space-
+			while which < len(piece) : # -1 ?
+				out = out + piece[which] + " "
+		#out[-1] = '\n' # replace extraneous space like a real man.
+		out = out[0:-1] + '\n'
+		return out
 
-def temp_r_chosen( symbol ) :
-	which_r = 9 - tr_left
-	tr_left -= 1
-	reg_name = "$t" + str( which_r ) + ","
-	used.append( (symbol, reg_name) ) # putting in a tuple
-	return reg_name
+	def assign( self, symbol ) :
+		if symbol in self.reserved :
+			return self.reserved[ symbol ]
+		# ugh, hacking in similarity to avoid functionality I'll put in later.
+		if symbol[-1] != ',' :
+			symbol = symbol + ","
+		# that's never become a problem later, hahaha.
+		if self.already_seen( symbol ) :
+			return self.prior_reservation( symbol )
+		elif self.t_reg_open() :
+			return self.temp_r_chosen( symbol )
+		elif self.s_reg_open() :
+			return self.sav_r_chosen( symbol )
+		else :
+			return put_in_stack( symbol )
 
-def sav_r_chosen( symbol ) :
-	which_r = 7 - sr_left
-	sr_left -= 1
-	reg_name = "$t" + str( which_r ) + ","
-	used.append( (symbol, reg_name) )
-	return reg_name
+	def t_reg_open( self ) :
+		return self.tr_left >= 0
+	def s_reg_open( self ) :
+		return self.sr_left >= 0
 
-def put_in_stack( symbol ) :
-	sp_next -= 4
-	place = "$sp-" + str( sp_next ) + ","
-	used.append( (symbol, place) )
-	return place
-	
+	def temp_r_chosen( self, symbol ) :
+		which_r = 9 - self.tr_left
+		self.tr_left -= 1
+		reg_name = "$t" + str( which_r ) + ","
+		self.used.append( (symbol, reg_name) ) # putting in a tuple
+		return reg_name
+
+	def sav_r_chosen( self, symbol ) :
+		which_r = 7 - self.sr_left
+		self.sr_left -= 1
+		reg_name = "$t" + str( which_r ) + ","
+		self.used.append( (symbol, reg_name) )
+		return reg_name
+
+	def put_in_stack( self, symbol ) :
+		self.sp_next -= 4
+		place = "$sp-" + str( self.sp_next ) + ","
+		self.used.append( (symbol, place) )
+		return place
+
+	def already_seen( self, symbol ) :
+		sym_spot = 0
+		for tup in self.used :
+			if tup[ sym_spot ] == symbol :
+				return True
+		return False
+
+	def prior_reservation( self, name ) :
+		for tup in self.used :
+			if tup[ 0 ] == name :
+				return tup[ 1 ]
 
 
 
@@ -158,9 +188,8 @@ def put_in_stack( symbol ) :
 
 
 
-
-
-
-
-
+bla = Alloc()
+bla.show()
+bla.should_trans()
+# auto( "sample.asm" )
 
